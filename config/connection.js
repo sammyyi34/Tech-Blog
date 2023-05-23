f1d55e66e46f69bd9ebd2f2bcc41d580c298c7ce
Sammy <sammyhyi@gmail.com>
2023-05-23 19:40:55 -0400

    made database

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 let sequelize;
 
+// deploys my database to the internet
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // normal sequelize call to connect to my database in my computer
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -13,7 +15,7 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3306 // the port where database is
     }
   );
 }
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const controllers = require('./controllers');
+const sequelize = require('./config/connection');
 
 const PORT = 3001;
 const app = express();
 
 app.use(controllers)
 
-app.listen(PORT, () => {
-  console.log(`Now listening at localhost:${PORT}`);
-})
+// connect to database and then to the server
+sequelize.sync({}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Now listening at localhost:${PORT}`);
+  })
+});
