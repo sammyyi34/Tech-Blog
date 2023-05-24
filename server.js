dc7ec2dabb1ab8350304a8a38a1d2ca6262a9692
Sammy <sammyhyi@gmail.com>
2023-05-23 22:28:00 -0400

    made main handlebars

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 const models = require('./models')
+const exphbs = require('express-handlebars');
 
 const PORT = 3001;
 const app = express();
 
+const hbs = exphbs.create({});
+
+// needed for handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// needed for post requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(controllers)
