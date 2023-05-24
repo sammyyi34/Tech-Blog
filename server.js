83f32079c6116faec349ea843f760119fec61c71
Sammy <sammyhyi@gmail.com>
2023-05-23 20:32:52 -0400

    made all models

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
+const models = require('./models')
 
 const PORT = 3001;
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(controllers)
 
 // connect to database and then to the server
-sequelize.sync({}).then(() => {
+sequelize.sync({force: false}).then(() => {
   app.listen(PORT, () => {
     console.log(`Now listening at localhost:${PORT}`);
   })
